3fc94fd36d8503cd3c4e595982fa2d13d83febdc
billy-kiramman <matias.maigua@alumnos.frm.utn.edu.ar>
2026-06-22 15:35:24 -0300

    Clase GenericService

--- src/foodstore/service/GenericService.java
+++ src/foodstore/service/GenericService.java
@@ -4,10 +4,34 @@
  */
 package foodstore.service;
 
+import foodstore.dao.IBaseDAO;
+import foodstore.exception.DAOException;
+import java.util.List;
+import java.util.Optional;
+
 /**
  *
  * @author usuario
  */
-public class GenericService {
-    
+public class GenericService<T> {
+    private IBaseDAO<T> dao;
+
+    public GenericService(IBaseDAO<T> dao) {
+        this.dao = dao;
+    }
+    public T crear(T entidad) throws DAOException{
+        return dao.crear(entidad);
+    }
+    public Optional<T> leer(Long id)throws DAOException{
+        return dao.leer(id);
+    }
+    public List<T> listar()throws DAOException{
+        return dao.listar();
+    }
+    public boolean actualizar(T entidad)throws DAOException{
+        return dao.actualizar(entidad);
+    }
+    public boolean eliminar(Long id)throws DAOException{
+        return dao.eliminar(id);
+    }
 }
