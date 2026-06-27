a641c82c3aa5c166999a06df6c62dfd9522dfe41
santistebanpilar-frm <pilarsg@pilarsg-Aspire-5742Z>
2026-06-27 02:50:13 -0300

    probadno si esta manera de codear la conexion funciona

--- src/config/ConexionDB.java
+++ src/config/ConexionDB.java
@@ -1,1 +1,44 @@
+package config;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.util.Properties;
+import java.io.IOException;
+import java.io.InputStream;
+
+public class ConexionDB {
+
+    private static ConexionDB instancia;
+
+    private String url;
+    private String user;
+    private String password;
+
+    private ConexionDB() {
+        try {
+            Properties props = new Properties();
+            InputStream is = getClass().getClassLoader()
+                    .getResourceAsStream("base.persistence");
+            props.load(is);
+
+            this.url = props.getProperty("db.url");
+            this.user = props.getProperty("db.user");
+            this.password = props.getProperty("db.password");
+
+        } catch (IOException e) {
+            System.out.println("Error al leer base.persistence: " + e.getMessage());
+        }
+    }
+
+    public static ConexionDB getInstance() {
+        if (instancia == null) {
+            instancia = new ConexionDB();
+        }
+        return instancia;
+    }
+
+    public Connection getConnection() throws SQLException {
+        return DriverManager.getConnection(url, user, password);
+    }
+}
