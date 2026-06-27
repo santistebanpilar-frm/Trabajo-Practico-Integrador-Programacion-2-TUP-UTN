c711b7c3f5a1c7a17c4a4c95207b8d2dbdf395ab
santistebanpilar-frm <pilarsg@pilarsg-Aspire-5742Z>
2026-06-27 02:46:36 -0300

    creacion de paquete exception + excepciones dentro

---
+++ src/exception/PersistenciaException.java
@@ -0,0 +1,15 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package exception;
+
+/**
+ *
+ * @author pilarsg
+ */
+public class PersistenciaException extends RuntimeException{
+    public PersistenciaException(String mensaje, Throwable causa) {
+        super(mensaje, causa);
+    }
+}
