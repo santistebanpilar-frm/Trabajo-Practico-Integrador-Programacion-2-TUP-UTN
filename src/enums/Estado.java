a121e1a9aedc67e82af575ed678fdae2c8bb271e
santistebanpilar-frm <pilarsg@pilarsg-Aspire-5742Z>
2026-06-27 02:48:54 -0300

    creacion de codigos de los enums

--- src/enums/Estado.java
+++ src/enums/Estado.java
@@ -1,1 +1,16 @@
+package enums;
+
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+
+/**
+ *
+ * @author pilarsg
+ */
+public enum Estado  {
+    PENDIENTE, CONFIRMADO, TERMINADO,CANCELADO, INVALIDO;
+}
 
--- src/enums/FormaPago.java
+++ src/enums/FormaPago.java
@@ -1,1 +1,17 @@
+package enums;
 
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+
+/**
+ *
+ * @author pilarsg
+ */
+public enum FormaPago  {
+    TARJETA,
+    TRANSFERECIA,
+    EFECTIVO;
+}
