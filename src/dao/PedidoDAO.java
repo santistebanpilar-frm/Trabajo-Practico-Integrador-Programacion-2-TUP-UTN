7094d5620981cee8e41e2a963f07022ed7ed67dc
santistebanpilar-frm <pilarsg@pilarsg-Aspire-5742Z>
2026-06-28 02:29:37 -0300

    codeo de daos: pedido

--- src/dao/PedidoDAO.java
+++ src/dao/PedidoDAO.java
@@ -4,10 +4,52 @@
  */
 package dao;
 
+import config.ConexionDB;
+import entities.DetallePedido;
+import entities.Pedido;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  *
  * @author pilarsg
  */
-public class PedidoDAO {
+public class PedidoDAO implements IBaseDAO<Pedido> {
+
+    @Override
+    public Pedido crear(Pedido entidad) {
+        throw new UnsupportedOperationException("Not supported yet."); 
+    }
+
+    @Override
+    public Pedido buscarPorId(Long id) {
+        throw new UnsupportedOperationException("Not supported yet."); 
+    }
+
+    @Override
+    public List<Pedido> listar() {
+        throw new UnsupportedOperationException("Not supported yet."); 
+    }
+    
+    public List<Pedido> listarPorUsuario() {
+        throw new UnsupportedOperationException("Not supported yet."); 
+    }
+
+    @Override
+    public boolean actualizar(Pedido entidad) {
+        throw new UnsupportedOperationException("Not supported yet."); 
+    }
+
+    @Override
+    public boolean eliminar(Long id) {
+        throw new UnsupportedOperationException("Not supported yet."); 
+    }
+    
+    private Pedido mapearPedido(ResultSet rs) throws SQLException{
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
     
 }
